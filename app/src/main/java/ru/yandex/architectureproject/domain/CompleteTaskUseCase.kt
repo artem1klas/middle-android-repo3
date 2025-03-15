bb82974a6acfd4c9cc5df6ece41d2a8b9d6394e2
artem1klas <baryshev964086@yandex.ru>
2025-03-15 23:10:23 +0300

    corrected comments

--- app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
+++ app/src/main/java/ru/yandex/architectureproject/domain/CompleteTaskUseCase.kt
@@ -6,9 +6,12 @@ import ru.yandex.architectureproject.data.repository.TaskRepository
 class CompleteTaskUseCase(
     private val repository: TaskRepository,
 ) {
+    companion object {
+        private const val DELAY_DELETE_TASK = 10000L
+    }
     suspend operator fun invoke(taskId: Int) {
         repository.completeTask(taskId)
-        delay(10000)
+        delay(DELAY_DELETE_TASK)
         repository.deleteTask(taskId)
     }
 }
